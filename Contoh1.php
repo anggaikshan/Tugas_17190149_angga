08b139f12d1ae5749894fec7be2d9b1af0befa0c
anggaikshan <59316517+anggaikshan@users.noreply.github.com>
2022-04-13 22:49:06 +0700

    Update Contoh1.php

--- Contoh1.php
+++ Contoh1.php
@@ -4,8 +4,8 @@ class Contoh1 extends CI_Controller
     public function index()
     {
         echo"<h1>Welcome to my blog </h1>";
-        echo"Nama saya Rehan Hardiansyah
-            Saya tinggal di daerah Bogor
+        echo"Nama saya angga 
+            Saya tinggal di daerah tangkot 
             olah raga yang saya sukai adalah
             Futsal";
     }
