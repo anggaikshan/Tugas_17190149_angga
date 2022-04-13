01f7a73b93a13d0faac45dce0af3e7c30e3446c0
anggaikshan <59316517+anggaikshan@users.noreply.github.com>
2022-04-13 22:47:49 +0700

    upload
    push

---
+++ Contoh1.php
@@ -0,0 +1,13 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo"<h1>Welcome to my blog </h1>";
+        echo"Nama saya Rehan Hardiansyah
+            Saya tinggal di daerah Bogor
+            olah raga yang saya sukai adalah
+            Futsal";
+    }
+    
+}
